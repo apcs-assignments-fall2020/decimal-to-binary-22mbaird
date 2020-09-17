90c9168c4df150027859cede12eb9556c8cc5384
22mbaird <22mbaird@athenian.org>
2020-09-17 16:55:20 +0000

    Completed code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 public class MyMain {
     
-    public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+    public static int binaryToDecimal(int binary) {
+        int dec = 0;  
+        int n = 0;  
+        while(true){  
+            if(binary == 0){  
+        break;  
+        } 
+        else {  
+            int k = binary%10;  
+            dec += k*Math.pow(2, n);  
+            binary = binary/10;  
+            n++;  
+        }  
+    }  
+    return dec;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Binary: ");
+        String x = scan.next();
+        System.out.println(Integer.parseInt(x, 2));  
+        scan.close();
     }
 }
